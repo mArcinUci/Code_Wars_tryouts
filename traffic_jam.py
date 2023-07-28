737d72179234dee08f31cb3992333f8368136203
Marcin Uciński <mucinski@poczta.onet.pl>
2023-07-28 17:03:45 +0200

    work in progress, but first C is in wrong place

--- traffic_jam.py
+++ traffic_jam.py
@@ -1,5 +1,6 @@
 def traffic_jam(main_road, side_streets):
-   active_str_num_and_cars_on_it = []
+   the_line = []
+   side_str_num_and_cars_on_it = []
 
    count_side_streets_cars = 0
 
@@ -9,22 +10,29 @@ def traffic_jam(main_road, side_streets):
    for i in range(how_many_side_streets):
       
       if len(side_streets[i])>0:
-         
-         active_str_num_and_cars_on_it.append([i,len(side_streets[i])])
+         side_str_num_and_cars_on_it.append([i,int(len(side_streets[i])),side_streets[i][0]])
          count_side_streets_cars += len(side_streets[i])
-         print('--->' + str(count_side_streets_cars))
          
    total_cars = len_main_road + count_side_streets_cars
-   how_many_side_streets_occupied = len(active_str_num_and_cars_on_it)
+   how_many_side_streets_occupied = len(side_str_num_and_cars_on_it)
+
+   while len(the_line) != total_cars:
+      if len(main_road) > 0:
+         the_line.append(main_road[0])
+         main_road = main_road[1:]
+
+         for i in range(how_many_side_streets_occupied):
+            if side_str_num_and_cars_on_it[i][1] > 0:
+               if main_road[side_str_num_and_cars_on_it[i][0]-1] != side_str_num_and_cars_on_it[i][2]:
+                  main_road = main_road[:side_str_num_and_cars_on_it[i][0]] + str(side_str_num_and_cars_on_it[i][2]) + main_road[side_str_num_and_cars_on_it[i][0]:]
+                  side_str_num_and_cars_on_it[i][1] -= 1
+         
+   the_line = ''.join(the_line)
 
    
-   
-
-
-   print(how_many_side_streets_occupied)
-   print(active_str_num_and_cars_on_it)
-   print(total_cars)
-
+   print(side_str_num_and_cars_on_it)
+   print(the_line)
+ 
 
 traffic_jam("abcdefghijklmX", ["","","","BBBBBB","","","","","CCCCC"])
 
