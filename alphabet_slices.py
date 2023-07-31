d905490936cc16becc8f0bf45ce3f02ef7bb70f5
Marcin Uciński <mucinski@poczta.onet.pl>
2023-07-31 13:43:47 +0200

    slicing started to work with one slice, not finding two slices, though

--- alphabet_slices.py
+++ alphabet_slices.py
@@ -1,19 +1,17 @@
 def solutions(s):
     ans = []
-    count = len(s)
     alphabet = 'abcdefghijklmnoprstqrstuvwxyz'
     for i in range(len(s)):
+        count = len(s)
         slice_s = s[i:count]
-        if slice_s in alphabet and count<=i:
+        if slice_s in alphabet and count>i:
             ans.append(slice_s)
         else:
             count -= 1
 
-    if 'abc' in alphabet:
-        print(1)
-    print(ans)
+    return ans
 
-solutions('xabc')
+print(solutions('xabc'))
 
 
 
