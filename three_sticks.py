545ab46771765f91e09b2d1d1b3e95949c91dfb7
Marcin Uciński <mucinski@poczta.onet.pl>
2023-08-09 10:34:59 +0200

    it does not work for (4,7) but works for (7,4)

--- three_sticks.py
+++ three_sticks.py
@@ -7,20 +7,21 @@ def maxlen(L1,L2):
     if L1 == L2:
         return L1 / 2
 
-    if L1 > L2 and L1/3 >= L2:
+    if L1 > L2 and L1/3 >= L2: # a stick 3x more than b
         return L1/3
-    if L1 > L2 and L2*2 < L1:
+    if L1 > L2 and L2*2 < L1 and L2*3 > L1:  # a stick 2-3 times more than b
         return L2
-    if L1 > L2 and L1/2 < L2:
+    if L1 > L2 and L1/2 < L2:  # a stick 1-2 times more than b
         return L1/2
     
     if L2 > L1 and L2/3 >= L1:
         return L2/3
-    if L2 > L1 and L1*2 < L2:
+    if L2 > L1 and L1*2 < L2 and L1*3 > L2:
         return L1
     if L2 > L1 and L2/2 < L1:
         return L1/2
             
 
 
-print(maxlen(12,5))
+print(maxlen(4,7))
+print(maxlen(7,4))
