943d18f57bd1eab044bdadcf0e10f8fa4372ae89
Marcin Uciński <mucinski@poczta.onet.pl>
2023-08-12 16:00:34 +0200

    passed all tests but to low internet on train to check properly all cases, Really nice challenge

--- coprimes.py
+++ coprimes.py
@@ -5,17 +5,18 @@ The list cannot include duplicated entries and has to be sorted.'''
 
 
 def coprimes(n):
-    list_of_numbers = [x for x in range(1,n) if n%x != 0]
-    list_of_coprimes = [1]
-    for i in list_of_numbers:
-        result = i
-        while result:
-            if i % result != 0 and n % result !=0:
-                list_of_coprimes.append(result)
-            result -= 1
-        
+    list_of_coprimes = [x for x in range(1,n) if n%x != 0]
+    list_of_codivisibles = []
+    for num in list_of_coprimes:
+        dummy = num
+        while dummy-1:
+            if n%dummy == 0 and num%dummy==0:
+                list_of_codivisibles.append(num)
+                break
+            dummy -= 1    
+    list_of_coprimes.append(1)
 
-
-    return list_of_coprimes
-
-print(coprimes(6))
+    final_list_of_coprimes = [x for x in list_of_coprimes if x not in list_of_codivisibles]
+    return sorted(final_list_of_coprimes)
+   
+print(coprimes(10))
