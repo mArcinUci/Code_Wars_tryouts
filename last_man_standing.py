a5df10174b1f771305f8c8faf9e416ef5fb0ed9a
Marcin Uciński <mucinski@poczta.onet.pl>
2023-08-14 16:41:32 +0200

    it works, kind of, not returning single digit  but a list - but there are good answeres in it:)

--- last_man_standing.py
+++ last_man_standing.py
@@ -6,21 +6,29 @@
 
 def last_man_standing(n):
     all_digits = [x for x in range(n+1)]
+    count = 0
     print(all_digits)
     if len(all_digits) == 2:
         return all_digits
     while len(all_digits) > 2:
-        first_element = all_digits[0]
         next_element = all_digits[1]
+        all_digits = all_digits[1:]
         all_digits = [x for x in all_digits if all_digits.index(x)%2 != all_digits.index(next_element)]
-        
-        print(all_digits)
-        if len(all_digits) > 2:
+        count +=1
+        print(f"cut from begining ------------->{all_digits}")
+        if len(all_digits) >2:
             last_element = all_digits[-1]
-            all_digits = [x for x in all_digits if all_digits.index(x)%2 != all_digits.index(last_element)%2]
-            #all_digits.remove(all_digits[-1])
-            print(all_digits)
-         
-    return all_digits
+            if len(all_digits)%2 == 0:
+                all_digits = [x for x in all_digits if not all_digits.index(x)%2 == all_digits.index(last_element)%2]
+                all_digits = all_digits[1:]
+                count+=1
+            else:
+                all_digits = [x for x in all_digits if not all_digits.index(x)%2 == all_digits.index(last_element)%2]
+                count+=1
+            print(f"cut from end========>{all_digits}")
+    if count%2==1:
+        return all_digits[0]
+    else:
+        return all_digits[1]
 
-print(last_man_standing(100))
+print(last_man_standing(9))
