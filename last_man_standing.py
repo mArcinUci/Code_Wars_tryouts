49aa57d55f306707e6f937bd6b4e1cb42c76060a
Marcin Uciński <mucinski@poczta.onet.pl>
2023-08-14 17:50:51 +0200

    works on 5 from 6 examples, do not know why do not work on 1000

--- last_man_standing.py
+++ last_man_standing.py
@@ -6,29 +6,31 @@
 
 def last_man_standing(n):
     all_digits = [x for x in range(n+1)]
-    count = 0
+    count = ''
     print(all_digits)
-    if len(all_digits) == 2:
-        return all_digits
     while len(all_digits) > 2:
         next_element = all_digits[1]
         all_digits = all_digits[1:]
         all_digits = [x for x in all_digits if all_digits.index(x)%2 != all_digits.index(next_element)]
-        count +=1
-        print(f"cut from begining ------------->{all_digits}")
-        if len(all_digits) >2:
+        count += 'L'
+        print(f"cut from begining ------------->{all_digits}----------{count}")
+        if len(all_digits) > 2:
             last_element = all_digits[-1]
             if len(all_digits)%2 == 0:
                 all_digits = [x for x in all_digits if not all_digits.index(x)%2 == all_digits.index(last_element)%2]
                 all_digits = all_digits[1:]
-                count+=1
+                count += 'R'
             else:
                 all_digits = [x for x in all_digits if not all_digits.index(x)%2 == all_digits.index(last_element)%2]
-                count+=1
-            print(f"cut from end========>{all_digits}")
-    if count%2==1:
+                count+= 'R'
+            print(f"cut from end========>{all_digits}--==========={count}")
+    if len(all_digits) == 1:
         return all_digits[0]
-    else:
-        return all_digits[1]
+    if len(all_digits) == 2:
+        if count[-1] == 'R':
+            return all_digits[1]
+        if count[-1] == 'L':
+            return all_digits[0]
 
-print(last_man_standing(9))
+
+print(last_man_standing(1000))
