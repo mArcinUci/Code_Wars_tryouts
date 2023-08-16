4e64461691085d8ef7f3ceb890341431e07f9a62
Marcin Uciński <mucinski@poczta.onet.pl>
2023-08-16 10:41:03 +0200

    nice and easy with list slicing, I have not been looking in a right place

--- last_man_standing.py
+++ last_man_standing.py
@@ -5,42 +5,22 @@
 
 
 def last_man_standing(n):
-    all_digits = [x for x in range(n+1)]
-    count = ''
+    all_digits = [x for x in range(1,n+1)]
     print(all_digits)
-    while len(all_digits) > 2:
-        next_element = all_digits[1]
-        all_digits = all_digits[1:]
-        # change here --> if len > 1 ??
-        all_digits = [x for x in all_digits if all_digits.index(x)%2 != all_digits.index(next_element)]
-        count += 'L'
-        print(f"cut from begining ------------->{all_digits}----------{count}")
-        if len(all_digits) > 2:
-            last_element = all_digits[-1]
-            if len(all_digits)%2 == 0:
-                all_digits = [x for x in all_digits if not all_digits.index(x)%2 == all_digits.index(last_element)%2]
-                all_digits = all_digits[1:]
-                count += 'R'
-            else:
-                all_digits = [x for x in all_digits if not all_digits.index(x)%2 == all_digits.index(last_element)%2]
-                count+= 'R'
-            print(f"cut from end========>{all_digits}--==========={count}")
-    if len(all_digits) == 1:
-        return all_digits[0]
-    if len(all_digits) == 2:
-        half = n/2
-        first_element_count = half - all_digits[0]
-        second_element_count = half - all_digits[1]
-        if n>100:
-            if abs(first_element_count) > abs(second_element_count):
-                return all_digits[1]
-            else:
-                return all_digits[0]
-        else:
-            if count[-1] == 'R':
-               return all_digits[1]
-            if count[-1] == 'L':
-                return all_digits[0]
+    while len(all_digits) > 1:
+        all_digits = all_digits[1::2]
+        print(all_digits)
+        if len(all_digits)>1:
+            all_digits = all_digits[::-1]
+            print(all_digits)
+            all_digits = all_digits[1::2]
+            print(all_digits)
+            all_digits = all_digits[::-1]
+            print(all_digits)
+
+    return all_digits
+
+        
 
 
 print(last_man_standing(1000))
