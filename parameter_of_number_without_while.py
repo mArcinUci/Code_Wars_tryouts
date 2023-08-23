7a150f92c7ed9465ef7428948975dd3881727fe2
Marcin Uciński <mucinski@poczta.onet.pl>
2023-08-23 22:00:45 +0200

    this did not work too :( --Execution Timed Out (12000 ms)--, went through moore examples though --85 vs 69

--- parameter_of_number_without_while.py
+++ parameter_of_number_without_while.py
@@ -5,14 +5,17 @@ Calculate the parameter of the given number n.'''
 
 def parameter(n):
     new_n = str(n).strip()
-    sum_of_digits_n = sum([int(x) for x in new_n])
-    almost_product_of_digits_n = [int(x) for x in new_n]
-    product_of_digits_n = 1
-    for i in almost_product_of_digits_n:
-        product_of_digits_n = product_of_digits_n * i
+    if len(new_n) == 1:
+        return n
+    else:
+        sum_of_digits_n = sum([int(x) for x in new_n])
+        almost_product_of_digits_n = [int(x) for x in new_n]
+        product_of_digits_n = 1
+        for i in almost_product_of_digits_n:
+            product_of_digits_n = product_of_digits_n * i
 
-    for currentPossibleLCM in range(max(sum_of_digits_n,product_of_digits_n), (sum_of_digits_n*product_of_digits_n)+1):
-         if((currentPossibleLCM % sum_of_digits_n == 0) and (currentPossibleLCM % product_of_digits_n == 0)):
-              return currentPossibleLCM
+        for currentPossibleLCM in range(max(sum_of_digits_n,product_of_digits_n), (sum_of_digits_n*product_of_digits_n)+1):
+            if((currentPossibleLCM % sum_of_digits_n == 0) and (currentPossibleLCM % product_of_digits_n == 0)):
+                return currentPossibleLCM
 
 print(parameter(1234))
