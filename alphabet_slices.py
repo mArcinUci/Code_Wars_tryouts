05d597a8c27ee65680a564030b3b1b4042b47822
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-01 14:27:12 +0200

    passed all tests, coll, added two more nice solutions

--- alphabet_slices.py
+++ alphabet_slices.py
@@ -1,4 +1,4 @@
-def solutions(s):
+def solution(s):
     dummy = None
     for i in range(len(s)-1):
         if ord(s[i])+1 == ord(s[i+1]):
@@ -12,7 +12,31 @@ def solutions(s):
     if dummy is not None:
         last_slice = s[dummy:]
         last_reversed_slice = last_slice[::-1]
-        s = s[:dummy]+last_reversed_slice
-        
+        s = s[:dummy]+last_reversed_slice     
     return s
-print(solutions('ggabcggabcffbcdxyz'))
+
+
+print(solution('ggabcggabcffbcdxyz'))
+
+
+
+'''def solution(s):
+    res, cur = [], [s[0]]
+    for x in s[1:]:
+        if ord(x) - ord(cur[-1]) == 1:
+            cur.append(x)
+        else:
+            res.extend(cur[::-1])
+            cur = [x]
+    
+    return ''.join(res + cur[::-1])'''
+
+'''
+def solution(s):
+    out = ""
+    i = 0
+    while len(out) < len(s):
+        rev_word, n = rev_ordered(s[i:])
+        out += rev_word
+        i += n
+    return out'''
