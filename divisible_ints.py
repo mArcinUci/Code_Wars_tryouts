87d97276ea271c0781936ed995cda6c38047c95f
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-03 15:42:27 +0200

    Done, nice task, passed all tests

---
+++ divisible_ints.py
@@ -0,0 +1,17 @@
+'''You are given an integer N. Your job is to figure out how many substrings inside of N divide evenly with N'''
+
+
+def get_count(n):
+    n = str(n)
+    all_possible_substrings_from_n = [n[i:j] for i in range(len(n))
+                                for j in range(i + 1, len(n) + 1)]
+    all_possible_ints_from_n = [int(x) for x in all_possible_substrings_from_n if int(x) != 0]
+    answer = 0
+    for i in all_possible_ints_from_n:
+        if int(n) % i == 0 and int(n) != i:
+            answer +=1
+    print(all_possible_ints_from_n)
+    return answer
+
+print(get_count(123))
+
