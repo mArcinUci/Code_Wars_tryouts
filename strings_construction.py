916b53da932b246c8f9cf3e06159e8e31da1db94
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-08 00:56:30 +0200

    changed logic, passed 100, failed 4

--- strings_construction.py
+++ strings_construction.py
@@ -4,22 +4,23 @@
 
 def strings_construction(a, b):
     how_many_letters_in_b = {}
-    count = 0
-    count_letters = 0
+    how_many_each_letters = []
     for i in b:
         if i in how_many_letters_in_b:
             how_many_letters_in_b[i] += 1
         else:
             how_many_letters_in_b[i] = 1
     
-    how_many_max_times_possible = len(b)//len(a)
-    for i in range(how_many_max_times_possible):
-        for x in a:
-            if x in how_many_letters_in_b.keys() and how_many_letters_in_b[x] > 0:
-                how_many_letters_in_b[x] -= 1
-                count_letters += 1
-            
-    return count_letters//len(a)
+    for x in a:
+        if x in how_many_letters_in_b.keys():
+            how_many_each_letters.append(how_many_letters_in_b[x])
+        else:
+            return 0
+               
+    if min(how_many_each_letters) > len(b)//len(a):
+        return len(b)//len(a)
+    else:
+        return min(how_many_each_letters)
 
 
-print(strings_construction('abcd','cbadeabcabcd'))
+print(strings_construction("abc","abccba"))
