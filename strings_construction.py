5a25f0548e201aa3340d5810117f774c74e298ee
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-08 09:40:48 +0200

    I found an issue, why 4 tests failed - ans in line 29-32

--- strings_construction.py
+++ strings_construction.py
@@ -10,17 +10,22 @@ def strings_construction(a, b):
             how_many_letters_in_b[i] += 1
         else:
             how_many_letters_in_b[i] = 1
-    
+    print(how_many_letters_in_b) 
+    print(how_many_each_letters)
+    print(len(b)/len(a))
+
     for x in a:
         if x in how_many_letters_in_b.keys():
             how_many_each_letters.append(how_many_letters_in_b[x])
         else:
             return 0
-               
+              
     if min(how_many_each_letters) > len(b)//len(a):
         return len(b)//len(a)
     else:
         return min(how_many_each_letters)
 
 
-print(strings_construction("abc","abccba"))
+print(strings_construction("hnuqnpowym",'pzqwpqhmupmwmbymywumpnkwuqjoum'))
+# so n is only 1 in b but 2 times in a, how to count that
+print(strings_construction("nlheprlp",'hheeppeerlpplherhnnlrhlrprpppellprnlnprrhlnrppnllh'))
