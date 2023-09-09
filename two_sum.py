330977547e933c328947af1f021bf0cd8dd530ee
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-09 15:54:56 +0200

    whatever i do, there are always 2-5 tests failed

---
+++ two_sum.py
@@ -0,0 +1,20 @@
+'''Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items
+ in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list
+   (depending on your language) like so: (index1, index2).
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; 
+target will always be the sum of two different items from that array).'''
+
+
+def two_sum(numbers, target):
+    for i in numbers:
+        for j in numbers:
+            if j + i == target and numbers.index(i) != numbers.index(j):
+                return (numbers.index(i), numbers.index(j))
+            if i + j == target and j == i:
+                indexes = [x for x,y in enumerate(numbers) if y == i]
+                return tuple(indexes)
+
+
+            
+print(two_sum([2,2,3],4))
