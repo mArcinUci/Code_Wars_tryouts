2c03cb9f7219fff4955ca85ca53beb3bca64a21a
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-10 09:44:43 +0200

    Done, two different code examples added

--- two_sum.py
+++ two_sum.py
@@ -11,7 +11,18 @@ def two_sum(numbers, target):
         for j in range(i+1,len(numbers)):
             if numbers[i] + numbers[j] == target:
                 return (i,j)
-
-
-            
+           
 print(two_sum([2,2,3],4))
+
+
+'''def two_sum(nums, t):
+    for i, x in enumerate(nums):
+        for j, y in enumerate(nums):
+            if i != j and x + y == t:
+                return [i, j]'''
+
+'''def two_sum(n, target):
+    for i in range(len(n)-1):
+        if (target-n[i]) in n[i+1:]:
+            return [i, n[i+1:].index(target-n[i])+(i+1)]
+    return None'''
