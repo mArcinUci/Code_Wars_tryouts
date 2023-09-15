f594a77ee3b0e0e66990f5364008b37bdf16b306
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-15 14:42:42 +0200

    it works fine for all cases where there ar no identical dice rolls or there are 2 identical, i have to add functionality when 3 or 4 rolls are the same

--- the_dice_game.py
+++ the_dice_game.py
@@ -4,37 +4,50 @@ If, despite these measures, we still have players with identical rolls in the ex
 Given a list of scores, documented meticulously in the order of each player's participation in every round, determine and declare the name of the triumphant player who stands as the last one standing.'''
 
 
-def dice_game(scores, winner=[]):
+def dice_game(scores, loosing_player=[]):
     overtime_1 = []
     new_scores = []
-    stamp = (4-len(winner))
+    stamp = (4-len(loosing_player))
     next_round = scores[:stamp]
     next_round_score = [sum(x) for x in next_round]
-    print(next_round_score)
-    
-    if len(winner)==3:
-        return winner 
-    
+
     for i in next_round_score:
         if min(next_round_score) == i:
             overtime_1.append(i)
     if len(overtime_1) == 1:
         loosing_score = min(next_round)
-        winner.append(next_round.index(loosing_score))
+        for i in range(next_round.index(loosing_score)+1, 5):
+            if i not in loosing_player:
+                loosing_player.append(i)
+                break
         new_scores = scores[stamp:]
     if len(overtime_1) == 2:
         loosing_score = [x for x in next_round if sum(x) == overtime_1[0]]
         if loosing_score[0][0] > loosing_score[1][0]:
-            winner.append(next_round.index(loosing_score[1]))
+            for i in range(next_round.index(loosing_score[1])+1, 5):
+                if i not in loosing_player:
+                    loosing_player.append(i)
+                    break
             new_scores =  scores[stamp:]           
         if loosing_score[0][0] < loosing_score[1][0]:
-            winner.append(next_round.index(loosing_score[0]))
+            for i in range(next_round.index(loosing_score[0])+1, 5):
+                if i not in loosing_player:
+                    loosing_player.append(i)
+                    break
             new_scores =  scores[stamp:]
         if loosing_score[0][0] == loosing_score[1][0]:
             new_scores =  scores[stamp:]
-    print(f'----and the looser here is {winner}')
-     
-    dice_game(new_scores, winner)
+    # here need to be answer to a sytuation where there are 3 or 4 equal 
+
+
+    if len(loosing_player)==3:
+        for i in range(1,5):
+            if i not in loosing_player:
+                and_the_winner_is = 'p'+str(i)
+                return and_the_winner_is
+    else:
+        return dice_game(new_scores, loosing_player)
     
   
-print(dice_game([(6, 2), (4, 3), (3, 4), (5, 4), (3, 5), (1, 5), (43, 3), (1, 5), (1, 5), (5, 6), (2, 2)])) 
+print(dice_game([(1, 5), (3, 1), (2, 3), (5, 3), (1, 2), (1, 2), (6, 3), (2, 2), (6, 3), (2, 2), (5, 5), (3, 1), (3, 1), (6, 6), (6, 4), (5, 3), (3, 4), (6, 4)]))
+print(dice_game([(3, 4), (2, 5), (5, 5), (2, 5), (6, 4), (6, 5), (6, 2), (6, 2), (3, 5), (6, 4), (4, 2), (5, 2), (3, 2), (6, 4), (1, 2), (5, 4), (5, 5)]))
