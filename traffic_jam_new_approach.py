9a4c366e06876be470422bb44ed63ef63fb31b6a
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-26 14:45:32 +0200

    problem with strings and lists --> when trying to solve it that way I will try tommorow:)

--- traffic_jam_new_approach.py
+++ traffic_jam_new_approach.py
@@ -1,43 +1,28 @@
 def traffic_jam(main_road, side_streets):
-    pointer = None
-    whoose_turn = 'start procedure'
-    sytuation_on_main_road = main_road
-    sytuation_on_side_streets = side_streets
+    last_line = []
+    pointer = ''
 
-    final_line = []
+    for i in side_streets:
+        main_pointer_index = side_streets.index(i)
+        if len(i)>0:
+            last_line = main_road[:main_pointer_index]
+            main_road = main_road[:main_pointer_index]
+            break
     
     while pointer != 'X':
-        if whoose_turn == 'start procedure':
-            for i in side_streets:
-                if len(i) > 0:
-                    starting_pointer = side_streets.index(i)
-                    break
-            for i in range(starting_pointer+1):
-                final_line.append(sytuation_on_main_road[i])
-                pointer = i
-            sytuation_on_main_road = sytuation_on_main_road[i+1:]
-            whoose_turn = 'main'
-                    
-
-        if whoose_turn == 'main':
-            print(final_line)
-            print(sytuation_on_main_road[0])
-            final_line.append(sytuation_on_main_road[0])
-            pointer = sytuation_on_main_road[0]
-            sytuation_on_main_road = sytuation_on_main_road[1:]
-            whoose_turn = 'side street'
-            break
-        if whoose_turn == 'side street':
-            for side_street_car in sytuation_on_side_streets:
-                if len(side_street_car) > 0 and side_street_car[0] != final_line[-1]: 
-                    side_street_car = side_street_car[1:]
-                    whoose_turn = 'main road'
-                    pointer = side_street_car[0]
-                    break
+        last_line.append(main_road[0])
+        main_road = main_road[1:]
+        pointer = str(main_road[0])
+        for i in side_streets:
+            if len(i) > 0 and i[0] != last_line[-1]:
+                last_line.append(i[0])
+                pointer = i[0]
+                i.pop[-1]
+                print(i)
+                break
     
-    return final_line
-
-
-print(traffic_jam("abcX", ["","","BBBBBB","","","","","CCCCC"]))
-                    
+        
+#problem with stings and lists --> when trying to solve it that way  
+    
+print(traffic_jam("abcdefghijklmX",  ["","","","BBBBBB","","","","","CCCCC"] ))
 
