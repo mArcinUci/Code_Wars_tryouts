4848d601c45ac24a6a4714d731d78c321b57d651
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-27 22:42:57 +0200

    passed 51 tests and -Execution Timed Out (12000 ms)-, but i fix my problem with overwriting with each recursion to the list

---
+++ cut_the_ropes.py
@@ -0,0 +1,29 @@
+'''
+You are given N ropes, where the length of each rope is a positive integer. At each step, you have to reduce all the ropes by the 
+length of the smallest rope.
+The step will be repeated until no ropes are left. Given the length of N ropes, print the number of ropes that are left 
+before each step.
+For a = [3, 3, 2, 9, 7], the result should be [5, 4, 2, 1]
+'''
+
+
+def cut_the_ropes(arr, ans = None):
+    if ans is None:
+        ans = []
+    if len(arr) > 0:
+        ans.append(len(arr))
+        min_lenght = min(arr)
+        
+        for i in arr:
+            if i - min_lenght > 0:
+                arr[arr.index(i)] = i - min_lenght
+            else:
+                arr[arr.index(i)] = 0
+        arr = [x for x in arr if x != 0]
+        return cut_the_ropes(arr,ans)
+    else:
+        return ans
+
+print(cut_the_ropes([3, 3, 2, 9, 7]))
+print(cut_the_ropes(([1, 2, 3, 4, 3, 3, 2, 1])))
+
