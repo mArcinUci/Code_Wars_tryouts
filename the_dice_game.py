2775a3d95146e68100074dc5ab48c41db3373775
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-28 10:55:19 +0200

    Done, passed all tests, yesterday i found how to fix problem with recursion, added two easier solutions below

--- the_dice_game.py
+++ the_dice_game.py
@@ -4,10 +4,12 @@ If, despite these measures, we still have players with identical rolls in the ex
 Given a list of scores, documented meticulously in the order of each player's participation in every round, determine and declare the name of the triumphant player who stands as the last one standing.'''
 
 
-def dice_game(scores, players = ['p1', 'p2', 'p3', 'p4']):
+def dice_game(scores, players = None):
     overtime_1 = []
     new_scores = []
     first_roll = []
+    if players is None:
+        players = ['p1', 'p2', 'p3', 'p4']
     stamp = len(players)
     next_round = scores[:stamp]
     next_round_score = [sum(x) for x in next_round]
@@ -33,11 +35,42 @@ def dice_game(scores, players = ['p1', 'p2', 'p3', 'p4']):
     new_scores = scores[stamp:]
     if len(players) == 1:
         return players[0]
-    else:
-       return dice_game(new_scores, players)
+    
+    return dice_game(new_scores, players)
     
   
-#print(dice_game([(1, 5), (3, 1), (2, 3), (5, 3), (1, 2), (1, 2), (6, 3), (2, 2), (6, 3), (2, 2), (5, 5), (3, 1), (3, 1), (6, 6), (6, 4), (5, 3), (3, 4), (6, 4)]))  # p3
-#print(dice_game(([(6, 3), (5, 5), (2, 3), (6, 6), (2, 5), (5, 1), (4, 4), (2, 2), (1, 3)]))) # p1
-#print(dice_game([(4, 4), (4, 3), (1, 1), (1, 1), (3, 1), (4, 5), (2, 6), (2, 3), (1, 5), (5, 3), (4, 5), (5, 2), (2, 1)]))  #p3
+
+
+print(dice_game([(1, 5), (3, 1), (2, 3), (5, 3), (1, 2), (1, 2), (6, 3), (2, 2), (6, 3), (2, 2), (5, 5), (3, 1), (3, 1), (6, 6), (6, 4), (5, 3), (3, 4), (6, 4)]))  # p3
+print(dice_game(([(6, 3), (5, 5), (2, 3), (6, 6), (2, 5), (5, 1), (4, 4), (2, 2), (1, 3)]))) # p1
+print(dice_game([(4, 4), (4, 3), (1, 1), (1, 1), (3, 1), (4, 5), (2, 6), (2, 3), (1, 5), (5, 3), (4, 5), (5, 2), (2, 1)]))  #p3
 print(dice_game(([(3, 4), (2, 5), (5, 5), (2, 5), (6, 4), (6, 5), (6, 2), (6, 2), (3, 5), (6, 4), (4, 2), (5, 2), (3, 2), (6, 4), (1, 2), (5, 4), (5, 5)]))) # p2
+
+
+
+
+'''
+def dice_game(scores):
+    players = ['p1', 'p2', 'p3', 'p4']
+    
+    while len(players) > 1:
+        results = list(zip(players, scores))
+        scores = scores[len(players):]
+        results.sort(key=lambda x: (sum(x[1]), x[1][0]))
+        if results[0][1] != results[1][1]:
+            players.remove(results[0][0])
+    return players[0]
+'''
+
+
+'''
+def dice_game(scores):
+    players = ['p1', 'p2', 'p3', 'p4']
+    while (i := len(players)) > 1:
+        state, scores = scores[:i], scores[i:]
+        x = sorted(state, key=lambda x: (sum(x), x[0]))
+        if sum(x[0]) != sum(x[1]) or x[0] != x[1]:
+            del players[state.index(x[0])]
+        
+    return players[0]
+'''
