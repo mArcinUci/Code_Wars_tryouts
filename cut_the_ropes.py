f10843433b531e884e52d0168384a248456eb93d
Marcin Uciński <mucinski@poczta.onet.pl>
2023-09-29 09:23:33 +0200

    Passed all tests, suprisingly a for loop did not trigger -timeout- command

--- cut_the_ropes.py
+++ cut_the_ropes.py
@@ -6,12 +6,13 @@ before each step.
 For a = [3, 3, 2, 9, 7], the result should be [5, 4, 2, 1]
 '''
 
-
 def cut_the_ropes(arr):
     answer = []
-    while len(arr)>0:
-        answer.append(len(arr))
-        arr = [x-min(arr) for x in arr if (x-min(arr)>0)]
+    for _ in range(len(arr)):
+        if len(arr) > 0:
+            answer.append(len(arr))
+            min_arr = min(arr)
+            arr = [x-min_arr for x in arr if (x-min_arr>0)]
     return answer
 
 print(cut_the_ropes([3, 3, 2, 9, 7]))
@@ -21,7 +22,7 @@ print(cut_the_ropes(([1, 2, 3, 4, 3, 3, 2, 1])))
 
 
 
-
+#one of my previous attempts
 '''
 def cut_the_ropes(arr, ans = None):
     if ans is None:
@@ -42,3 +43,28 @@ def cut_the_ropes(arr, ans = None):
 '''
 
 
+#somehow this one worked (and my while loop was with -timeout problem-
+'''
+def cut_the_ropes(a):
+    result = []
+    while a:
+        result.append(len(a))
+        m = min(a)
+        a = [x-m for x in a if x > m]
+    return result'''
+
+
+'''
+def cut_the_ropes(arr):
+    qty = len(arr)
+    sorted_ropes = sorted(arr)
+    return [qty - sorted_ropes.index(n) for n in sorted(set(arr))]
+'''
+
+'''
+def cut_the_ropes(a):
+    if not a:
+        return []
+    m = min(a)
+    return [len(a)] + cut_the_ropes([x-m for x in a if x > m])
+'''
