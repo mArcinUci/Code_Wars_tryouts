b2e29232248f0c75aaf4ce957d4ddd8fc7f097c1
Marcin Uciński <mucinski@poczta.onet.pl>
2023-10-03 12:08:52 +0200

    i think i found source of a problem --16 line----> printed data shows where--- tomorrow i will work on solution

--- traffic_jam.py
+++ traffic_jam.py
@@ -12,6 +12,9 @@ def traffic_jam(main_road, side_streets):
         final_line.append(the_main_road[0])
         the_main_road = the_main_road[1:]
         for i in range(len(side_streets)):
+            if i >= len(main_road)-2:
+                print(f'---{len(final_line)} --i: {i}    ---side streets:{len(side_streets)}    ---len the main road {len(the_main_road)} =={the_main_road[-1]}=== ----side_str[i][0] {side_streets[i][0]}' )
+                break
             if i == 0 and len(side_streets[0])>0 and side_streets[0][0] != final_line[-1]:
                the_main_road = [side_streets[0][0]] + the_main_road
                side_streets[i] = side_streets[0][1:]               
@@ -20,7 +23,7 @@ def traffic_jam(main_road, side_streets):
                the_main_road = the_main_road[:i] + [str(side_streets[i][0])] + the_main_road[i:]
                side_streets[i] = side_streets[i][1:]
                break
-  
+            
     for x in range(len(final_line)):
         if len(final_line[x]) > 1:
             final_line[x] = final_line[x][1]
@@ -29,15 +32,14 @@ def traffic_jam(main_road, side_streets):
     final_line = ''.join(final_line)
     return final_line
 
-      
-#to pass all tests I need to differentiate beetween main and side streets (it is not always so main is lower and side is upper) 
 
-print(traffic_jam("abcdefX", []), "abcdefX")
-print(traffic_jam("abcXdef", []), "abcX")
-print(traffic_jam("Xabcdef", []), "X")
-print(traffic_jam("abcdefghijklmX", ["","","","BBBBBB","","","","","CCCCC"]), "abcdBeBfBgBhBiBCjCkClCmCX")
-print(traffic_jam("abcdeXghi", ["","","CCCCC","","EEEEEEEEEE","FFFFFF","","","IIIIII"]), "abcCdCeCECX")
+
+#print(traffic_jam("abcdefX", []), "abcdefX")
+#print(traffic_jam("abcXdef", []), "abcX")
+#print(traffic_jam("Xabcdef", []), "X")
+#print(traffic_jam("abcdefghijklmX", ["","","","BBBBBB","","","","","CCCCC"]), "abcdBeBfBgBhBiBCjCkClCmCX")
+#print(traffic_jam("abcdeXghi", ["","","CCCCC","","EEEEEEEEEE","FFFFFF","","","IIIIII"]), "abcCdCeCECX")
 #print(traffic_jam("abcdefghijklmX", ["","","","BBBBBB","","","","","CCCCC"]), "abcdBeBfBgBhBiBCjCkClCmCX")
 
 
-#traffic_jam("abcdefghijklmnopqrstuvwX", ["AAA","BBB","CCC", "DDD","EEE", "FFF", "GGG", "HHH", "III", "JJJ", "KKK", "LLL", "MMM", "NNN", "OOO", "PPP", "QQQ", "RRR", "SSS", "TTT", "UUU", "VVV", "WWW"])
+traffic_jam("abcdefghijklmnopqrstuvwX", ["AAA","BBB","CCC", "DDD","EEE", "FFF", "GGG", "HHH", "III", "JJJ", "KKK", "LLL", "MMM", "NNN", "OOO", "PPP", "QQQ", "RRR", "SSS", "TTT", "UUU", "VVV", "WWW"])
