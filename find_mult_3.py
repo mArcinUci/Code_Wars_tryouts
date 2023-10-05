f16cf2db5a44a7fde6f8ffe22848f825f489551c
Marcin Uciński <mucinski@poczta.onet.pl>
2023-10-05 23:27:18 +0200

    Passed 59 or 72 tests, then-> Testing for num = 40742162 or num = 83590376 -->Execution Timed Out (12000 ms)

--- find_mult_3.py
+++ find_mult_3.py
@@ -1,6 +1,6 @@
 '''
 Given a certain number, how many multiples of three could you obtain with its digits?
-We need a function that can receive a number ann may output in the following order:
+We need a function that can receive a number and may output in the following order:
 -the amount of multiples (that are multiple of three)
 -the maximum multiple
 '''
@@ -9,19 +9,17 @@ We need a function that can receive a number ann may output in the following ord
 
 from itertools import permutations
 
-
-
 def find_mult_3(num):
-    all_perm = []
-    num = str(num)
-    for i in range(1,len(num)+1):
-        all_perm += list(permutations(num,i))
-    
-    all_perms_str = [''.join(str(e) for e in perm) for perm in all_perm]
-    all_perms_str = [int(x) for x in all_perms_str]
-    all_perms_str = list(set(all_perms_str))
-    all_perms_int_div_by_3 = [x for x in all_perms_str if x%3 ==0 and x != 0]
-    
+    num_str = str(num)
+    all_perms = []
+
+    for r in range(1, len(num_str) + 1):
+        all_perms += list(permutations(num_str, r))
+
+    all_perms_int = [int(''.join(perm)) for perm in all_perms]
+    all_perms_int = set(all_perms_int)
+    all_perms_int_div_by_3 = [x for x in all_perms_int if x % 3 == 0 and x != 0]
+
     return [len(all_perms_int_div_by_3), max(all_perms_int_div_by_3)]
 
 print(find_mult_3(362))
