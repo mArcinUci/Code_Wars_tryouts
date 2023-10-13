0aa0295af85db5a4b2b222bf2a0a0a94421dfe39
Marcin Uciński <mucinski@poczta.onet.pl>
2023-10-13 10:44:36 +0200

    Done in O(n) time, all test passed, i discovered this solution an my own - marcin proud

--- penalty_for_speeding.py
+++ penalty_for_speeding.py
@@ -6,6 +6,30 @@ Your penalty charge is a combination of numbers like: speed of your car, speed l
 For example, if you are given numbers [45, 30, 50, 1] your best choice is 1304550
 '''
 
+def penalty(a_list):
+    lenght_list = [len(x) for x in a_list]
+    uniq_lenght_list = list(set(lenght_list))
+    common_product = 1
+    for x in uniq_lenght_list:
+        common_product = common_product * x
+    product_for_every_item = [int(common_product/x) for x in lenght_list]
+    final_list = [a*b for a,b in zip(a_list,product_for_every_item)]
+    common_list = zip(final_list, a_list)
+    common_list = sorted(common_list)
+    min_penalty = ''
+    for i in common_list:
+        min_penalty += i[1]
+    return min_penalty
+    
+
+
+print(penalty(['45', '30', '50', '1']))  # 1304550 
+print(penalty(['100', '10', '1']))       # 100101
+print(penalty(['71', '82', '42', '34', '90']))  # 3442718290
+print(penalty(['31', '97', '6', '78']))  # 3167897
+
+# my other (bigger time complexity) solution
+'''
 from itertools import permutations
 
 def penalty(a_list):
@@ -16,8 +40,36 @@ def penalty(a_list):
     min_penalty = str(min(final_list_of_all_permutations))
     
     return min_penalty
+'''
 
-print(penalty(['45', '30', '50', '1']))  # 1304550 
-print(penalty(['100', '10', '1']))       # 100101
-print(penalty(['71', '82', '42', '34', '90']))  # 3442718290
-print(penalty(['31', '97', '6', '78']))  # 3167897
+#solutions from CodeWars
+
+'''
+# return str of the smallest value of the combined numbers in a_list
+# the length of a_list can vary betweem 2 and 20  
+def penalty(a_list):
+    return ''.join(sorted(a_list, key=lambda x: x+x))
+'''
+
+
+'''
+# return str of the smallest value of the combined numbers in a_list
+# the length of a_list can vary betweem 2 and 20  
+def penalty(a_list):
+    return ''.join(sorted(sorted(a_list, key=lambda x: x[-1]), key=lambda x: x[0]))
+'''
+
+
+'''
+# return str of the smallest value of the combined numbers in a_list
+# the length of a_list can vary betweem 2 and 20  
+def val_num(x):
+    suma =0
+    for i, y in enumerate(x, start=1):
+        suma += i*int(y)
+    return x * 10 *len(x)
+def penalty(a_list):
+    print(sorted(a_list,key=lambda x: (val_num, len)))
+    return "".join(sorted(a_list,key=val_num))
+#     return "".join(sorted(a_list, key=lambda x: sum(ord(y) for y in x)))
+'''
