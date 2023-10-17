3dda3050d7e99ffc9549c1d72209478c9700ccde
Marcin Uciński <mucinski@poczta.onet.pl>
2023-10-17 14:33:24 +0200

    just started but have to live that for tommorow

---
+++ basic_DeNico.py
@@ -0,0 +1,34 @@
+'''
+description on: https://www.codewars.com/kata/596f610441372ee0de00006e/train/python
+'''
+
+def de_nico(key, msg):
+    return ""
+massage = 'abcdefghijk'
+key = 'nico'
+f = list(enumerate(key))
+z = list(enumerate(sorted(key)))
+
+
+print(f)
+print(z)
+
+'''
+my plan:
+- rearrange key and according to that rearrange msg using len(msg) sublists
+but my brain does not work today after all work i have don so far, i will try tommorow
+'''
+
+
+
+
+
+
+
+'''
+Examples:
+deNico("crazy", "cseerntiofarmit on  ") => "secretinformation"
+deNico("abc", "abcd") => "abcd"
+deNico("ba", "2143658709") => "1234567890"
+deNico("key", "eky") => "key" 
+'''
