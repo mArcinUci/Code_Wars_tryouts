31ec3806c2d28714090bfa358bb8e79ced78d4e2
Marcin Uciński <mucinski@poczta.onet.pl>
2023-10-17 18:40:54 +0200

    kid of works, but only for subsets == len(key) and i cannot wrap it up in function - because of generator

--- basic_DeNico.py
+++ basic_DeNico.py
@@ -1,8 +1,8 @@
 '''
 description on: https://www.codewars.com/kata/596f610441372ee0de00006e/train/python
 '''
-massage = 'abcdefghijkl'
-key = 'nico'
+massage = "2143658709"
+key = "ba"
 chunk_size = len(key)
 ready_chunks = []
 sorted_key = sorted(key)
@@ -10,10 +10,6 @@ new_order_in_chunks = [int(key.index(x)) for x in sorted_key]
 
 def de_nico(key, msg):
     pass
-   
-   
-
-
 
 def chunks(some_string, chunk_size):
     string_size = len(some_string)
@@ -21,29 +17,32 @@ def chunks(some_string, chunk_size):
         yield some_string[x: min(x+chunk_size, string_size)]
 
 for chunk in chunks(massage, chunk_size):
-    new_chunk = chunk
-    for i in new_order_in_chunks:
-        print(f'-------{new_chunk[new_order_in_chunks.index(i)]}--------')
-        print(chunk[i])    
-        new_chunk.replace(new_chunk[new_order_in_chunks.index(i)], chunk[i])
-        print(f'======={new_chunk}======')
-        #new_chunk[new_order_in_chunks.index(i)] = chunk[i]
-        break
+    new_chunk = ''.join(chunk[i] for i in new_order_in_chunks)
     ready_chunks.append(new_chunk)
-
-
+ready_chunks = ''.join(ready_chunks)
 print(ready_chunks)
-
-
+# it works (for substr always equal to len(key)) but i cannot make it work when close it into function - look bellow:
 '''
-my plan:
-- rearrange key and according to that rearrange msg using len(msg) sublists
-but my brain does not work today after all work i have don so far, i will try tommorow
+def de_nico(key, some_string):
+
+    chunk_size = len(key)
+    string_size = len(some_string)
+    for x in range(0, string_size, chunk_size):
+        yield some_string[x: min(x+chunk_size, string_size)]
+
+    ready_chunks = []
+    chunk_size = len(key)
+    sorted_key = sorted(key)
+    new_order_in_chunks = [int(key.index(x)) for x in sorted_key]
+
+    for chunk in de_nico(some_string, chunk_size): 
+        chunk = ''.join(chunk[i] for i in new_order_in_chunks)
+        ready_chunks.append(chunk)
+        print(ready_chunks)
+    return list(ready_chunks)
+
+print(de_nico('niko', '123412341234'))
 '''
-
-
-
-
 
 
 
